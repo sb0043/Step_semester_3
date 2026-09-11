4f08d5579adfe9c1f20bb0e15eb9fdff68ba05ad
sb0043 <sb0043@srmist.edu.in>
2026-09-11 23:47:58 +0530

    Add class problem 3 BookInventory

---
+++ src/main/java/oop/class_problems/BookInventory.java
@@ -0,0 +1,28 @@
+package oop.class_problems;
+
+public class BookInventory {
+
+    private int copiesTotal;
+    private int copiesAvailable;
+
+    public BookInventory(int copiesTotal) {
+        this.copiesTotal = copiesTotal;
+        this.copiesAvailable = copiesTotal;
+    }
+
+    public void checkOut() {
+        if (copiesAvailable > 0) {
+            copiesAvailable--;
+        }
+    }
+
+    public void checkIn() {
+        if (copiesAvailable < copiesTotal) {
+            copiesAvailable++;
+        }
+    }
+
+    public int getCopiesAvailable() {
+        return copiesAvailable;
+    }
+}
