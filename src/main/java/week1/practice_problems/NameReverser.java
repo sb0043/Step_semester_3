04271cf03771cef37a0d6543698163081c69a28b
sb0043 <sb0043@srmist.edu.in>
2026-09-19 16:39:08 +0530

    Add customer name reverser practice problem

---
+++ src/main/java/week1/practice_problems/NameReverser.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class NameReverser {
+
+    public static String reverseCustomerName(String customerName) {
+
+        String reversed = "";
+
+        for (int i = customerName.length() - 1; i >= 0; i--) {
+            reversed = reversed + customerName.charAt(i);
+        }
+
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter customer name: ");
+        String customerName = scanner.nextLine();
+
+        String reversedName = reverseCustomerName(customerName);
+
+        System.out.println("Original Name: " + customerName);
+        System.out.println("Reversed Name: " + reversedName);
+
+        scanner.close();
+    }
+}
