bea546dcaa815694211741d97714181b200a7350
sb0043 <sb0043@srmist.edu.in>
2026-09-22 17:07:28 +0530

    Add student static members practice problem

---
+++ src/main/java/week3/practice_problems/Student.java
@@ -0,0 +1,29 @@
+package week3.practice_problems;
+
+public class Student {
+
+    String name;
+    int attendance;
+
+    static String collegeName = "SRM Institute of Science and Technology";
+    static int studentCount = 0;
+
+    public Student(String name, int attendance) {
+        this.name = name;
+        this.attendance = attendance;
+        studentCount++;
+    }
+
+    static void printCollegeInfo() {
+        System.out.println(collegeName);
+        System.out.println("Students created: " + studentCount);
+    }
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Ravi", 90);
+        Student student2 = new Student("Anitha", 95);
+
+        Student.printCollegeInfo();
+    }
+}
