18b071a33290c4cac62ac9443cb38b919d77418d
sb0043 <sb0043@srmist.edu.in>
2026-09-22 17:11:22 +0530

    Add employee static members assignment problem

---
+++ src/main/java/week3/assignment_problems/EmployeeStatic.java
@@ -0,0 +1,35 @@
+package week3.assignment_problems;
+
+public class EmployeeStatic {
+
+    String empName;
+    double salary;
+
+    static String companyName = "Bright Horizon Technologies";
+    static int employeeCount = 0;
+
+    public EmployeeStatic(String empName, double salary) {
+        this.empName = empName;
+        this.salary = salary;
+        employeeCount++;
+    }
+
+    static void printCompanyInfo() {
+        System.out.println(companyName);
+        System.out.println("Employees on record: " + employeeCount);
+    }
+
+    public static void main(String[] args) {
+
+        EmployeeStatic employee1 =
+                new EmployeeStatic("Divya", 65000);
+
+        EmployeeStatic employee2 =
+                new EmployeeStatic("Arjun", 40000);
+
+        EmployeeStatic employee3 =
+                new EmployeeStatic("Karthik", 50000);
+
+        EmployeeStatic.printCompanyInfo();
+    }
+}
